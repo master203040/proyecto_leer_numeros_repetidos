c453a784cbef548a2feccf226f15b5a0126e13ec
carlos ortega <carlosalbertoortegac@gmail.com>
2022-07-22 22:11:34 -0500

    proyecto para leer numeros repetidos y duplicados

---
+++ ConsoleApp1/programa2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class programa2
+    {
+        public void Main(string[] agrs) {
+
+            int[] myArray = { 1, 2, 1, 3, 3, 1, 2, 1, 5, 1 };
+            int[] lista = new int[myArray.Length];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < myArray.Length; j++)
+                {
+                    if (myArray[i] == myArray[j])
+                    {
+                        count++;
+                    }
+
+                    if (Numero(myArray[j]))
+                    {
+                        lista[i] = myArray[j];
+                    }
+
+                }
+                if (lista[i] != 0)
+                {
+                    Console.WriteLine(lista[i] + "se repite:" + count);
+
+                }
+
+            }
+            bool Numero(int num)
+            {
+                for (int i = 0; i < lista.Length; i++)
+                {
+                    if (lista[i] == num)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+        }
+
+
+    }
+}
+    
---
+++ ConsoleApp1/proyecto2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class proyecto2
+    {
+        static void Main(string[] args) 
+        {
+            int[] myArray = { 1, 3, 9, 3, 9, 1, 2};
+            bool repley = false;
+            for (var x = 0; x < myArray.Length; x++)
+            {
+                int a = myArray[x];
+                int c = x +1;
+                for (int y = c; y< myArray.Length; y++)
+                {
+
+                    int b = myArray[y];
+                    if (a == b)
+                    {
+                        repley = true;
+                        System.Console.WriteLine("en numero " + a + "esta repetido en la lista ");
+                    }
+
+                }
+            
+                
+            }
+            if (repley==false)
+            {
+                System.Console.WriteLine("no hay numeros repetidos en la lista ");
+            
+            }
+
+
+        }
+
+    }
+}
